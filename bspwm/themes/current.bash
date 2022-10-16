60f79961e31c3a54f397e75e3b85a3b6535aeef1
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-10-16 20:55:59 +0530

    updated bspwm config

--- bspwm/themes/current.bash
+++ bspwm/themes/current.bash
@@ -1,34 +1,34 @@
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
 # Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
 #
-# Nord color scheme
-# ------------------------------------------------------------------------------
+# Blade color scheme
+# -----------------------------------------------------------------------------
 
 # Wallpaper
-wallpaper='/usr/share/backgrounds/nord.jpg'
+wallpaper='/usr/share/backgrounds/blade.jpg'
 
 # Special
-background='#2E3440'
-foreground='#D8DEE9'
-cursor='#D8DEE9'
+foreground='#f1c4c0'
+background='#0c1018'
+cursor='#f1c4c0'
 
 # Colors
-color0='#3B4252'
-color1='#BF616A'
-color2='#A3BE8C'
-color3='#EBCB8B'
-color4='#81A1C1'
-color5='#B48EAD'
-color6='#88C0D0'
-color7='#E5E9F0'
-color8='#4C566A'
-color9='#BF616A'
-color10='#A3BE8C'
-color11='#EBCB8B'
-color12='#81A1C1'
-color13='#B48EAD'
-color14='#8FBCBB'
-color15='#ECEFF4'
+color0='#20242C'
+color1='#F8506D'
+color2='#F06576'
+color3='#CF916E'
+color4='#FA3C82'
+color5='#E9698E'
+color6='#EA7288'
+color7='#f1c4c0'
+color8='#a88986'
+color9='#F8506D'
+color10='#F06576'
+color11='#CF916E'
+color12='#FA3C82'
+color13='#E9698E'
+color14='#EA7288'
+color15='#f1c4c0'
 
 # Don't Delete This File
