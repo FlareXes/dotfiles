137ded3f51079bbc0d1a1019d56a61b8e9542923
FlareXes <70066527+FlareXes@users.noreply.github.com>
2023-04-19 08:49:14 +0530

    add firefox user.js overrides

---
+++ user-overrides.js
@@ -0,0 +1,6 @@
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("keyword.enabled", true);
+
+
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
