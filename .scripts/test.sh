2a69c13b199c6a58ffd448124b47880565d7427c
FlareXes <70066527+FlareXes@users.noreply.github.com>
2023-09-01 09:59:42 +0530

    Added New Linux Security Scripts

---
+++ .scripts/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/firejail --appimage --profile=/home/radowoo/.config/firejail/fluent-reader.profile /home/radowoo/Applications/Fluent.Reader.1.1.3_e2c1b298158c7f5a7e96b49aeee9d5c3.AppImage
