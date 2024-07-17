9d0dcdda3142f39729f462ed72e5ffdc34daf246
FlareXes <70066527+FlareXes@users.noreply.github.com>
2024-07-17 17:11:58 +0530

    delete: remove unnecessary scripts

--- .scripts/chang-hardware-id.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Regenerate machine ID
-rm -f /var/lib/dbus/machine-id
-dbus-uuidgen > /var/lib/dbus/machine-id
-cp -p /var/lib/dbus/machine-id /etc/machine-id
-
-chmod 444 /etc/machine-id
-
