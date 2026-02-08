07bfcc473cf6167082e141e0425e1068c718358e
FlareXes <70066527+FlareXes@users.noreply.github.com>
2026-02-08 19:09:19 +0530

    move to ~/.local/bin

--- .scripts/open-morgen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-active_window=$(hyprctl activewindow | awk '/class:/ {print $2}')
-
-if [[ "$active_window" == "Morgen" ]]; then
-    hyprctl dispatch killactive 
-else
-    morgen
-fi
