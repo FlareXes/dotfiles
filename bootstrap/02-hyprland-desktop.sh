aac1fb7c4863b91236ce105926aec3537fadc6ea
FlareXes <70066527+FlareXes@users.noreply.github.com>
2026-04-24 03:54:22 +0530

    feat: add thunar with extensions

--- bootstrap/02-hyprland-desktop.sh
+++ bootstrap/02-hyprland-desktop.sh
@@ -9,6 +9,7 @@ pac_install \
   xdg-desktop-portal-hyprland \
   qt5-wayland qt6-wayland \
   rofi rofi-calc \
+  thunar gvfs tumbler ffmpegthumbnailer thunar-archive-plugin \
   polkit-gnome \
   udiskie \
   nwg-look \
